f7d797b4afdc152038b7f8f19f0522fd2dab9ddb
urbanIntrovert <trefoil72@gmail.com>
2025-11-25 19:13:02 +0000

    chore(settings): enable DEBUG mode for development

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d6qa^d#w5ym1oqlx209+tf&yj@2r%gj5@tu2b(zdda%^4!co!z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
