73737d6545f7b1fa890fe53a749090db12ddd705
urbanIntrovert <trefoil72@gmail.com>
2025-11-25 16:03:54 +0000

    feat: prepare Django project for Heroku deployment
    - added DISABLE_COLLECTSTATIC=1 config var on Heroku
    - installed gunicorn~=20.1 and updated requirements.txt
    - created Procfile with gunicorn startup command
    - added '.herokuapp.com' to ALLOWED_HOSTS
    - added local development host (127.0.0.1) to ALLOWED_HOSTS after DisallowedHost error
    - updated DEBUG setting to False for production deployment
    - verified project still runs locally

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d6qa^d#w5ym1oqlx209+tf&yj@2r%gj5@tu2b(zdda%^4!co!z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
